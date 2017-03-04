2d62de76a52d61fc71cefdf3345cc6f69c7480ef
Rishabh Jain <monu.monujain@gmail.com>
2017-03-05 03:28:35 +0530

    Update Parser.hs
    Added relational operations although chaining is not allowed so only one relational operation in one statement.

--- Assignments/Interpreter/Parser.hs
+++ Assignments/Interpreter/Parser.hs
@@ -1,5 +1,6 @@
 --Imports
 import Data.Char
+import Data.List
 
 --Reserved words in julia
 listOfReservedWords = ["if",
@@ -19,6 +20,9 @@ data Token = Operator String
            | Assign
            | Identifier String
            | TokEnd   deriving (Show)
+
+--helper function to tell if any string is contained in another
+isSubstring ls hs = isInfixOf ls hs
 
 --Function to return a constant type token from stream and return the rest of stream
 getNumberToken :: String -> (Token, String)
@@ -41,8 +45,14 @@ skipWhitespace :: String -> String
 skipWhitespace xs = dropWhile isSpace xs
 
 --Function to check if character is an Operator
-isOperator :: Char -> Bool
-isOperator ch = any (\x -> x == ch) "+-*/"
+isOperator :: String -> Bool
+isOperator oper = any (\x -> x == oper) ["+","-","*","/","%","^","<",">","<=",">=","==","!="]
+
+getOperator :: String -> (Token, String)
+getOperator (x:y:ls)
+  | isOperator ([x]++[y]) = (Operator ([x]++[y]), ls)
+  | isOperator [x] = (Operator [x], [y]++ls)
+  | otherwise = error "Some Awkard state happened at getOperator"
 
 --Function which takes a token as an input and return which operator the token is...
 whichOperator :: Token -> String
@@ -51,7 +61,8 @@ whichOperator (Operator s)
   |s == "-" = "MINUS"
   |s == "*" = "MULT"
   |s == "/" = "DIV"
-  |otherwise = "None"
+  |s == "%" = "MOD"
+  |otherwise = s
 whichOperator _ = "None"
 
 --Checks if the given String matches with a reserved word
@@ -65,16 +76,17 @@ tokenise [] = [TokEnd]
 tokenise (x:xs)
   | x =='(' = [LPar] ++ tokenise (xs)
   | x ==')' = [RPar] ++ tokenise (xs)
-  | x == '=' = [Assign] ++ tokenise (xs)
+  | isAlpha x && isKeyword (takeWhile isAlpha (x:xs)) = [Keyword (takeWhile isAlpha (x:xs))] ++ tokenise (dropWhile isAlpha (x:xs))
   | isAlpha x = (\p -> [Identifier (fst p)] ++ tokenise (snd p)) (getIdentifier (x:xs))
   | isDigit x = (\p -> [fst p] ++ tokenise (snd p)) (getNumberToken (x:xs))
   | isSpace x = tokenise (skipWhitespace xs)
-  | isOperator x = [Operator [x]] ++ tokenise (xs)
+  | isOperator [x] || isOperator ([x]++[head xs]) = let (operToken, restOfStream) = getOperator (x:xs) in [operToken] ++ tokenise restOfStream
+  | x == '=' = [Assign] ++ tokenise (xs)
   | otherwise = []
 
 --Parse tree Data Structure
-data Tree = SumNode String Tree Tree
-          | ProdNode String Tree Tree
+data Tree = ANode String Tree Tree
+          | RNode String Tree Tree
           | AssignNode String Tree
           | NumNodeDouble Double
           | NumNodeInt Int
@@ -95,22 +107,36 @@ hasMoreTokens :: [Token] -> Bool
 hasMoreTokens (x : _) = undefined
 
 --Parser - we implement our grammar rules here
+-- logicalExpr -> expr [ ('<'|'>'|'<='|'>='|'!='|'==') expr]*
 -- expr -> term ['+'|'-' term]* | Identifier '=' expr
--- term -> factor ['*'|'/' factor]*
+-- term -> factor ['*'|'/'|'%' factor]*
 -- factor -> '(' expr ')' | identifier | number
+
+
+helperlogicalExpr :: (Tree, [Token]) -> (Tree, [Token])
+helperlogicalExpr (ltree, toks) =
+  let (exprTree, toks') = expr (eatToken toks) in
+    case whichOperator (getToken toks) of ">"    -> helperlogicalExpr (RNode ">" ltree exprTree, toks')
+                                          "<"    -> helperlogicalExpr (RNode "<" ltree exprTree, toks')
+                                          ">="   -> helperlogicalExpr (RNode ">=" ltree exprTree, toks')
+                                          "<="   -> helperlogicalExpr (RNode "<=" ltree exprTree, toks')
+                                          "=="   -> helperlogicalExpr (RNode "==" ltree exprTree, toks')
+                                          "!="   -> helperlogicalExpr (RNode "!=" ltree exprTree, toks')
+                                          _      -> (ltree, toks)
 
 helperterm :: (Tree, [Token]) -> (Tree, [Token])
 helperterm (ltree, toks) =
     let (factTree, toks') = factor (eatToken toks) in
-      case whichOperator (getToken toks) of "MULT"-> helperterm (ProdNode "MULT" ltree factTree, toks')
-                                            "DIV" -> helperterm (ProdNode "DIV" ltree factTree, toks')
+      case whichOperator (getToken toks) of "MULT"-> helperterm (ANode "MULT" ltree factTree, toks')
+                                            "DIV" -> helperterm (ANode "DIV" ltree factTree, toks')
+                                            "MOD" -> helperterm (ANode "MOD" ltree factTree, toks')
                                             _     -> (ltree, toks)
 
 helperexpr ::(Tree, [Token]) -> (Tree, [Token])
 helperexpr (ltree, toks) =
   let (termTree, toks') = term (eatToken toks) in
-    case whichOperator (getToken toks) of "PLUS"-> helperexpr (SumNode "PLUS" ltree termTree, toks')
-                                          "MINUS" -> helperexpr (SumNode "MINUS" ltree  termTree, toks')
+    case whichOperator (getToken toks) of "PLUS"-> helperexpr (ANode "PLUS" ltree termTree, toks')
+                                          "MINUS" -> helperexpr (ANode "MINUS" ltree  termTree, toks')
                                           _     -> (ltree, toks)
 
 term :: [Token] ->  (Tree, [Token])
@@ -133,23 +159,34 @@ expr toks =
                                                                                    in (AssignNode s expTree, toks')
                         _            -> helperexpr (term toks)
 
+logicalExpr :: [Token] -> (Tree, [Token])
+logicalExpr toks = helperlogicalExpr (expr toks)
+
 parse:: [Token] -> Tree
-parse toks = let (tree, toks') = expr toks
+parse toks = let (tree, toks') = logicalExpr toks
              in
                 if (show (getToken toks')) == "TokEnd" then tree else error $ "parsing error: leftover tokens" ++ (show toks')
 
 evalTree:: Tree -> Int
+--Multiple relational operations can be parsed but can't be executed
 --evalTree (VarNode x) = find value of x from map(or list)
 evalTree (NumNodeInt x) = x
-evalTree (SumNode op leftTree rightTree) = case op of "PLUS" -> (evalTree leftTree + evalTree rightTree)
-                                                      "MINUS"-> (evalTree leftTree - evalTree rightTree)
-                                                      _      -> error "Unknown Operator at SumNode"
-evalTree (ProdNode op leftTree rightTree) = case op of "MULT" -> (evalTree leftTree * evalTree rightTree)
-                                                       "DIV"  -> (evalTree leftTree `div` evalTree rightTree)
-                                                       _      -> error "Unkown Operator at ProdNode"
+evalTree (ANode op leftTree rightTree) = case op of "PLUS" -> (evalTree leftTree + evalTree rightTree)
+                                                    "MINUS"-> (evalTree leftTree - evalTree rightTree)
+                                                    "MULT" -> (evalTree leftTree * evalTree rightTree)
+                                                    "DIV"  -> (evalTree leftTree `div` evalTree rightTree)
+                                                    "MOD"  -> (evalTree leftTree `rem` evalTree rightTree)
+                                                    _      -> error "Unkown Operator at ANode eval"
+evalTree (RNode op leftTree rightTree) = case op of "<"    -> if (evalTree leftTree < evalTree rightTree) then 1 else 0
+                                                    ">"    -> if (evalTree leftTree < evalTree rightTree) then 1 else 0
+                                                    "<="   -> if (evalTree leftTree < evalTree rightTree) then 1 else 0
+                                                    ">="   -> if (evalTree leftTree < evalTree rightTree) then 1 else 0
+                                                    "=="   -> if (evalTree leftTree < evalTree rightTree) then 1 else 0
+                                                    "!="   -> if (evalTree leftTree < evalTree rightTree) then 1 else 0
+                                                    _      -> error "Unkown Operator at RNode eval"
 evalTree _ = error "Awkward evaluation state"
 
 
 main = do
   --print (evalTree (parse (tokenise " 1 / 2 ")))
-  print ((evalTree.parse.tokenise) " 2-(4-2*2-5-7*3)/6/(((2))) ")
+  print ((evalTree.parse.tokenise) "1%2 == 1 ")
